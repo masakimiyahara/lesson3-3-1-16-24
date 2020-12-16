11b688cafb43f37598de0f3025d8467eb0b089e8
EC2 Default User <ec2-user@ip-172-31-52-253.ec2.internal>
2020-12-16 14:44:48 +0000

    変更した

--- lesson3-3-1.js
+++ lesson3-3-1.js
@@ -2,28 +2,28 @@
 $("input[name = 'search_input']").keyup(function(){
   let serchText = $(this).val();
   console.log("serchText")
-  let target_1 = document.getElementsBy("image-text_1");
-  if (target_1.indexOf(serchText)>= 0) {
-        $(".image-content_1").removeClass("addhidden");
+   let target_1 = $(".image-text_1").text();
+  if (target_1.indexOf(serchText) >= 0) {
+        $(".image-content_1").removeClass("hidden");
     } else {
-        $(".image-content_1").addClass("addhidden");
+        $(".image-content_1").addClass("hidden");
     }
-  let target_2 = document.getElementById(".image-text_2");
+  let target_2 = $(".image-text_2").text();
   if (target_2.indexOf(serchText)>= 0) {
-        $(".image-content_2").removeClass("addhidden");
+        $(".image-content_2").removeClass("hidden");
     } else {
-        $(".image-content_2").addClass("addhidden");
+        $(".image-content_2").addClass("hidden");
     }
-    let target_3 = document.getElementById(".image-text_3");
+    let target_3 = $(".image-text_3").text();
   if (target_3.indexOf(serchText)>= 0) {
-        $(".image-content_3").removeClass("addhidden");
+        $(".image-content_3").removeClass("hidden");
     } else {
-        $(".image-content_3").addClass("addhidden");
+        $(".image-content_3").addClass("hidden");
     }
-    let target_4 = document.getElementById(".image-text_4");
+    let target_4 = $(".image-text_4").text();
   if (target_4.indexOf(serchText)>= 0) {
-        $(".image-content_4").removeClass("addhidden");
+        $(".image-content_4").removeClass("hidden");
     } else {
-        $(".image-content_4").addClass("addhidden");
+        $(".image-content_4").addClass("hidden");
     }
 });
